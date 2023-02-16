2179cb9dffa1fa9cca73ae28a7d8cac3615c4593
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-02-16 21:07:11 +0530

    [Ayushi] Added find frequency of word in a sentence

--- HashTableAndBinarySearchTree/Program.cs
+++ HashTableAndBinarySearchTree/Program.cs
@@ -11,7 +11,38 @@ namespace HashTableAndBinarySearchTree
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to hash table programming");
+
+            Console.WriteLine("1. Add Data");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    string para = "To be or not to be";
+                    CountNumbOfOccurence(para);
+                    break;
+                default:
+                    Console.WriteLine("Please Enter the correct choice");
+                    break;
+            }
             Console.ReadLine();
+        }
+        public static void CountNumbOfOccurence(string paragraph)
+        {
+            MyNode<string, int> hashTabe = new MyNode<string, int>(6);
+            string[] words = paragraph.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (hashTabe.Exists(word.ToUpper()))
+                    hashTabe.Add(word.ToUpper(), hashTabe.Get(word.ToUpper()) + 1);
+                else
+                    hashTabe.Add(word.ToUpper(), 1); 
+            }
+            Console.WriteLine(" Frequency of words in sentence");
+            hashTabe.Display();
         }
     }
 }
+  
+
+
